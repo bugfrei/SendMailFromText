735068848e5cdf64b53096604492654d39ab92a1
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2023-11-08 11:50:51 +0100

    Clipboard changes wasn't saved :-(

--- batch.ps1
+++ batch.ps1
@@ -11,7 +11,9 @@ if ($TextFile -eq "")
 {
     $c = (Get-Clipboard)
 }
-$c = (Get-Content $TextFile -Raw)
+else {
+    $c = (Get-Content $TextFile -Raw)
+}
 $adrList = [regex]::Matches($c, "([@-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+)", [System.Text.RegularExpressions.RegexOptions]::Singleline).Value
 
 $att = (Get-Item $Attachment).FullName
